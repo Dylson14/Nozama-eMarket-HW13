eb4b1e4d04dde4f871176dfa06be851d6a997b6a
Dylson Oliveira <dylsonuni@gmail.com>
2022-02-14 20:41:15 +0000

    correcting the post routes to just req.body

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -56,10 +56,7 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new category
-  Category.create({
-    category_name: req.body.category_name
-  })
-  .then(dbCatData => res.json(dbCatData))
+  Category.create(req.body).then(dbCatData => res.json(dbCatData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
